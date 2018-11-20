9f6b406ef9553d0e1d4ab4e2c47fe342d42370f5
amaturi2525 <pee.llgm@gmail.com>
2018-11-20 13:07:26 +0900

    gen test

--- generate_data.py
+++ generate_data.py
@@ -1,1 +1,25 @@
 from PIL import Image
+import os, glob
+import numpy as np
+from sklearn import cross_validation
+
+classes = ["monkey"]
+num_classes = len(classes)
+image_size = 50
+
+#画像の読み込み
+X = []
+Y = []
+for index, classlabel in enumerate(classes):
+    photos_dir = "./animal_picture/" + classlabel
+    files = glob.glob(photos_dir + "/*.jpg")
+    for i, file in enumerate(files):
+        if i >= 200:break
+        image = Image.open(file)
+        image = image.convert("RGB")
+        image = image.resize((image_size, image_size))
+        data = np.asarray(image)
+        X.append(data)
+        Y.append(index)
+X = np.array(X)
+Y = np.array(Y)
